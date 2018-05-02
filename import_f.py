4968a2ba2d3a922c9f6788d184f996e50e80951e
dlotnykPC <dmytro.lotnyk@gmail.com>
2018-05-02 11:08:25 -0400

    Two graphs added

--- import_f.py
+++ import_f.py
@@ -15,7 +15,7 @@ import numpy as np
 path = "d:\\therm_transport\\thermal_cond\\FF2mKheat.dat"
 num_exp=10 # number of point around pulse to remove
 
-data=np.genfromtxt(path, unpack=True, skip_header=1, usecols = (2, 5, 6, 13))
+data=np.genfromtxt(path, unpack=True, skip_header=1, usecols = (2, 5, 6, 13, 7))
 """ 0 - time; 1 - frequency; 2 - Q; 3 - Tmct"""
 data[0] = data[0]-data[0][0] 
 
@@ -29,11 +29,21 @@ d=np.in1d(range(0,len(data[0])),b,assume_unique=True,invert = True)
 
 Q=data[2][d]
 time=data[0][d]
+F=data[4][d]
+T=data[3][d]
 # plotting
-plt.plot(time, Q)
-plt.xlabel('time (s)')
-plt.ylabel('Q')
-plt.title('Q vs time')
-plt.grid(True)
-#plt.savefig("test.png")
+fig1 = plt.figure(1, clear = True)
+ax1 = fig1.add_subplot(111)
+ax1.set_ylabel('Q')
+ax1.set_xlabel('time')
+ax1.set_title('Q vs time')
+line, = ax1.plot(time, Q, color='blue', lw=2)
+plt.show()
+
+fig2 = plt.figure(2, clear = True)
+ax2 = fig2.add_subplot(111)
+ax2.set_ylabel('Q')
+ax2.set_xlabel('temperature')
+ax2.set_title('Q vs temperature')
+line, = ax2.plot(T, Q, color='blue', lw=2)
 plt.show()
